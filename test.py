fdc5ecd31db5515bc1e3380880362ff8daced708
Yoshihiro Kita <kita.yoshihiro@gmail.com>
2018-09-15 16:34:52 +0900

    a

--- test.py
+++ test.py
@@ -1,4 +1,54 @@
+import keras
 from keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 from sklearn.model_selection import train_test_split
 x_train1, x_valid, y_train1, y_valid = train_test_split(x_train, y_train, test_size=0.175)
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+# 28 x 28の画像がgrayscaleで1chなので、28, 28, 1にreshapeする
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_valid = x_valid.reshape(x_valid.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+# 0-255の整数値を0〜1の小数に変換する
+# MNISTって必ずこの処理入るけれど、意味あるのかな
+x_train = x_train.astype('float32')
+x_valid = x_valid.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_valid /= 255
+x_test /= 255
+
+# one-hot vector形式に変換する
+y_train = keras.utils.to_categorical(y_train, 10)
+y_valid = keras.utils.to_categorical(y_valid, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+from keras.optimizers import RMSprop
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=128,
+                    epochs=10,
+                    verbose=1,
+                    validation_data=(x_valid, y_valid))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
