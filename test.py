46c8d7706f5142387c62250c8446e64c81f9bdbb
Yoshihiro Kita <kita.yoshihiro@gmail.com>
2018-09-15 16:27:37 +0900

    a

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print('hello2')
+from keras.datasets import mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+from sklearn.model_selection import train_test_split
+x_train1, x_valid, y_train1, y_valid = train_test_split(x_train, y_train, test_size=0.175)
