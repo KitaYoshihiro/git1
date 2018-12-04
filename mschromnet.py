aa6468aa3a7815c50ada75eeb2ba25307b44843e
Yoshihiro Kita <kita.yoshihiro@gmail.com>
2018-12-04 20:07:33 +0900

    a

--- mschromnet.py
+++ mschromnet.py
@@ -6,6 +6,7 @@ from keras.layers import AtrousConvolution1D
 from keras.layers import Conv1D
 from keras.layers import Dense
 from keras.layers import Flatten
+from keras.layers import Concatenate
 from keras.layers import GlobalAveragePooling1D
 from keras.layers import Input
 from keras.layers import MaxPooling1D
@@ -16,6 +17,9 @@ from keras.layers import merge
 from keras.layers import Reshape
 from keras.layers import ZeroPadding1D
 from keras.models import Model
+
+from mschromnet_layers import PriorBox
+from mschromnet_layers import Normalize
 
 def Conv1DBNRelu(input, net, basename, cn):
     """CNN1D
@@ -45,7 +49,7 @@ def Upsample1D(input, net, basename):
     net['upsample' + basename] =  UpSampling1D(name='upsample' + basename)(input)
     return net['upsample' + basename]    
 
-def MSChromNet(input_shape):
+def MSChromNet(input_shape, num_classes=2):
     """SSD-like 1D architecture
     """
     net = {}
@@ -61,78 +65,202 @@ def MSChromNet(input_shape):
     x = Conv1DBNRelu(x, net, '2_1', 32)
     x = Conv1DBNRelu(x, net, '2_2', 32)
     x = Conv1DBNRelu(x, net, '2_3', 32)
+    net['conv2_3_norm'] = Normalize(20, name='conv2_3_norm')(x)
+    num_priors = 3
+    net['conv2_3_norm_mbox_loc'] = Conv1D(num_priors * 2, 3,
+                        padding='same',
+                        name='conv2_3_norm_mbox_loc')(net['conv2_3_norm'])
+    net['conv2_3_norm_mbox_loc_flat'] = Flatten(name='conv2_3_norm_mbox_loc_flat')(net['conv2_3_norm_mbox_loc'])
+    net['conv2_3_norm_mbox_conf'] = Conv1D(num_priors * num_classes, 3,
+                        padding='same',
+                        name='conv2_3_norm_mbox_conf')(net['conv2_3_norm'])
+    net['conv2_3_norm_mbox_conf_flat'] = Flatten(name='conv2_3_norm_mbox_conf_flat')(net['conv2_3_norm_mbox_conf'])
+    net['conv2_3_norm_mbox_priorbox'] = PriorBox(input_shape[0], 8.0,
+                        aspect_ratios=[2],
+                        variances=[0.1, 0.2],
+                        name='conv2_3_norm_mbox_priorbox')(net['conv2_3_norm'])
     x = MaxPool1D(x, net, '2')
-    # x = Upsample1D(x, net, '2')
 
     x = Conv1DBNRelu(x, net, '3_1', 64)
     x = Conv1DBNRelu(x, net, '3_2', 64)
     x = Conv1DBNRelu(x, net, '3_3', 64)
+    num_priors = 5
+    net['conv3_3_mbox_loc'] = Conv1D(num_priors * 2, 3,
+                        padding='same',
+                        name='conv3_3_mbox_loc')(x)
+    net['conv3_3_mbox_loc_flat'] = Flatten(name='conv3_3_mbox_loc_flat')(net['conv3_3_mbox_loc'])
+    net['conv3_3_mbox_conf'] = Conv1D(num_priors * num_classes, 3,
+                        padding='same',
+                        name='conv3_3_mbox_conf')(x)
+    net['conv3_3_mbox_conf_flat'] = Flatten(name='conv3_3_mbox_conf_flat')(net['conv3_3_mbox_conf'])
+    net['conv3_3_mbox_priorbox'] = PriorBox(input_shape[0], 16.0,
+                        aspect_ratios=[2, 3],
+                        variances=[0.1, 0.2],
+                        name='conv3_3_mbox_priorbox')(x)
     x = MaxPool1D(x, net, '3')
-    # x = Upsample1D(x, net, '2')
 
     x = Conv1DBNRelu(x, net, '4_1', 128)
     x = Conv1DBNRelu(x, net, '4_2', 128)
     x = Conv1DBNRelu(x, net, '4_3', 128)
+    num_priors = 5
+    net['conv4_3_mbox_loc'] = Conv1D(num_priors * 2, 3,
+                        padding='same',
+                        name='conv4_3_mbox_loc')(x)
+    net['conv4_3_mbox_loc_flat'] = Flatten(name='conv4_3_mbox_loc_flat')(net['conv4_3_mbox_loc'])
+    net['conv4_3_mbox_conf'] = Conv1D(num_priors * num_classes, 3,
+                        padding='same',
+                        name='conv4_3_mbox_conf')(x)
+    net['conv4_3_mbox_conf_flat'] = Flatten(name='conv4_3_mbox_conf_flat')(net['conv4_3_mbox_conf'])
+    net['conv4_3_mbox_priorbox'] = PriorBox(input_shape[0], 32.0,
+                        aspect_ratios=[2, 3],
+                        variances=[0.1, 0.2],
+                        name='conv4_3_mbox_priorbox')(x)
     x = MaxPool1D(x, net, '4')
-    # x = Upsample1D(x, net, '4')
 
     x = Conv1DBNRelu(x, net, '5_1', 256)
     x = Conv1DBNRelu(x, net, '5_2', 256)
     x = Conv1DBNRelu(x, net, '5_3', 256)
+    num_priors = 5
+    net['conv5_3_mbox_loc'] = Conv1D(num_priors * 2, 3,
+                        padding='same',
+                        name='conv5_3_mbox_loc')(x)
+    net['conv5_3_mbox_loc_flat'] = Flatten(name='conv5_3_mbox_loc_flat')(net['conv5_3_mbox_loc'])
+    net['conv5_3_mbox_conf'] = Conv1D(num_priors * num_classes, 3,
+                        padding='same',
+                        name='conv5_3_mbox_conf')(x)
+    net['conv5_3_mbox_conf_flat'] = Flatten(name='conv5_3_mbox_conf_flat')(net['conv5_3_mbox_conf'])
+    net['conv5_3_mbox_priorbox'] = PriorBox(input_shape[0], 64.0,
+                        aspect_ratios=[2, 3],
+                        variances=[0.1, 0.2],
+                        name='conv5_3_mbox_priorbox')(x)
     x = MaxPool1D(x, net, '5')
-    # x = Upsample1D(x, net, '5')
 
     x = Conv1DBNRelu(x, net, '6_1', 512)
     x = Conv1DBNRelu(x, net, '6_2', 512)
     x = Conv1DBNRelu(x, net, '6_3', 512)
+    num_priors = 5
+    net['conv6_3_mbox_loc'] = Conv1D(num_priors * 2, 3,
+                        padding='same',
+                        name='conv6_3_mbox_loc')(x)
+    net['conv6_3_mbox_loc_flat'] = Flatten(name='conv6_3_mbox_loc_flat')(net['conv6_3_mbox_loc'])
+    net['conv6_3_mbox_conf'] = Conv1D(num_priors * num_classes, 3,
+                        padding='same',
+                        name='conv6_3_mbox_conf')(x)
+    net['conv6_3_mbox_conf_flat'] = Flatten(name='conv6_3_mbox_conf_flat')(net['conv6_3_mbox_conf'])
+    net['conv6_3_mbox_priorbox'] = PriorBox(input_shape[0], 128.0,
+                        aspect_ratios=[2, 3],
+                        variances=[0.1, 0.2],
+                        name='conv6_3_mbox_priorbox')(x)
     x = MaxPool1D(x, net, '6')
-    # x = Upsample1D(x, net, '6')
 
     x = Conv1DBNRelu(x, net, '7_1', 1024)
     x = Conv1DBNRelu(x, net, '7_2', 1024)
     x = Conv1DBNRelu(x, net, '7_3', 1024)
+    num_priors = 5
+    net['conv7_3_mbox_loc'] = Conv1D(num_priors * 2, 3,
+                        padding='same',
+                        name='conv7_3_mbox_loc')(x)
+    net['conv7_3_mbox_loc_flat'] = Flatten(name='conv7_3_mbox_loc_flat')(net['conv7_3_mbox_loc'])
+    net['conv7_3_mbox_conf'] = Conv1D(num_priors * num_classes, 3,
+                        padding='same',
+                        name='conv7_3_mbox_conf')(x)
+    net['conv7_3_mbox_conf_flat'] = Flatten(name='conv7_3_mbox_conf_flat')(net['conv7_3_mbox_conf'])
+    net['conv7_3_mbox_priorbox'] = PriorBox(input_shape[0], 256.0,
+                        aspect_ratios=[2, 3],
+                        variances=[0.1, 0.2],
+                        name='conv7_3_mbox_priorbox')(x)
     x = MaxPool1D(x, net, '7')
 
-    x = Conv1DBNRelu(x, net, '8_1', 2048)
-    x = Conv1DBNRelu(x, net, '8_2', 2048)
-    x = Conv1DBNRelu(x, net, '8_3', 2048)
-    x = Upsample1D(x, net, '8')
+    x = Conv1DBNRelu(x, net, '8_1', 1024) # ここは2048ではなく1024にして節約
+    x = Conv1DBNRelu(x, net, '8_2', 1024)
+    x = Conv1DBNRelu(x, net, '8_3', 1024)
+    num_priors = 5
+    net['conv8_3_mbox_loc'] = Conv1D(num_priors * 2, 3,
+                        padding='same',
+                        name='conv8_3_mbox_loc')(x)
+    net['conv8_3_mbox_loc_flat'] = Flatten(name='conv8_3_mbox_loc_flat')(net['conv8_3_mbox_loc'])
+    net['conv8_3_mbox_conf'] = Conv1D(num_priors * num_classes, 3,
+                        padding='same',
+                        name='conv8_3_mbox_conf')(x)
+    net['conv8_3_mbox_conf_flat'] = Flatten(name='conv8_3_mbox_conf_flat')(net['conv8_3_mbox_conf'])
+    net['conv8_3_mbox_priorbox'] = PriorBox(input_shape[0], 512.0,
+                        aspect_ratios=[2, 3],
+                        variances=[0.1, 0.2],
+                        name='conv8_3_mbox_priorbox')(x)
 
+    x = Upsample1D(x, net, '9')
     x = Conv1DBNRelu(x, net, '9_1', 1024)
     x = Conv1DBNRelu(x, net, '9_2', 1024)
     x = Conv1DBNRelu(x, net, '9_3', 1024)
-    x = Upsample1D(x, net, '9')
 
+    x = Upsample1D(x, net, '10')
     x = Conv1DBNRelu(x, net, '10_1', 512)
     x = Conv1DBNRelu(x, net, '10_2', 512)
     x = Conv1DBNRelu(x, net, '10_3', 512)
-    x = Upsample1D(x, net, '10')
 
+    x = Upsample1D(x, net, '11')
     x = Conv1DBNRelu(x, net, '11_1', 256)
     x = Conv1DBNRelu(x, net, '11_2', 256)
     x = Conv1DBNRelu(x, net, '11_3', 256)
-    x = Upsample1D(x, net, '11')
 
+    x = Upsample1D(x, net, '12')
     x = Conv1DBNRelu(x, net, '12_1', 128)
     x = Conv1DBNRelu(x, net, '12_2', 128)
     x = Conv1DBNRelu(x, net, '12_3', 128)
-    x = Upsample1D(x, net, '12')
 
+    x = Upsample1D(x, net, '13')
     x = Conv1DBNRelu(x, net, '13_1', 64)
     x = Conv1DBNRelu(x, net, '13_2', 64)
     x = Conv1DBNRelu(x, net, '13_3', 64)
-    x = Upsample1D(x, net, '13')
 
+    x = Upsample1D(x, net, '14')
     x = Conv1DBNRelu(x, net, '14_1', 32)
     x = Conv1DBNRelu(x, net, '14_2', 32)
     x = Conv1DBNRelu(x, net, '14_3', 32)
-    x = Upsample1D(x, net, '14')
 
+    x = Upsample1D(x, net, '15')
     x = Conv1DBNRelu(x, net, '15_1', 16)
     x = Conv1DBNRelu(x, net, '15_2', 16)
     x = Conv1DBNSigmoid(x, net, '15_3', 1)
-    net['flatten15'] = Flatten()(x) 
+    net['flatten15'] = Flatten()(x)
 
+    # Gather Predictions
+
+    net['mbox_loc'] = Concatenate(name='mbox_loc', axis=1)([net['conv2_3_norm_mbox_loc_flat'],
+                            net['conv3_3_mbox_loc_flat'],
+                            net['conv4_3_mbox_loc_flat'],
+                            net['conv5_3_mbox_loc_flat'],
+                            net['conv6_3_mbox_loc_flat'],
+                            net['conv7_3_mbox_loc_flat'],
+                            net['conv8_3_mbox_loc_flat']])
+    net['mbox_conf'] = Concatenate(name='mbox_conf', axis=1)([net['conv2_3_norm_mbox_conf_flat'],
+                            net['conv3_3_mbox_conf_flat'],
+                            net['conv4_3_mbox_conf_flat'],
+                            net['conv5_3_mbox_conf_flat'],
+                            net['conv6_3_mbox_conf_flat'],
+                            net['conv7_3_mbox_conf_flat'],
+                            net['conv8_3_mbox_conf_flat']])
+    net['mbox_priorbox'] = Concatenate(name='mbox_priorbox', axis=1)([net['conv2_3_norm_mbox_priorbox'],
+                            net['conv3_3_mbox_priorbox'],
+                            net['conv4_3_mbox_priorbox'],
+                            net['conv5_3_mbox_priorbox'],
+                            net['conv6_3_mbox_priorbox'],
+                            net['conv7_3_mbox_priorbox'],
+                            net['conv8_3_mbox_priorbox']])
+    if hasattr(net['mbox_loc'], '_keras_shape'):
+        num_boxes = net['mbox_loc']._keras_shape[-1] // 2
+    elif hasattr(net['mbox_loc'], 'int_shape'):
+        num_boxes = K.int_shape(net['mbox_loc'])[-1] // 2
+
+    net['mbox_loc'] = Reshape((num_boxes, 2),
+                              name='mbox_loc_final')(net['mbox_loc'])
+    net['mbox_conf'] = Reshape((num_boxes, num_classes),
+                               name='mbox_conf_logits')(net['mbox_conf'])
+    net['mbox_conf'] = Activation('softmax',
+                                  name='mbox_conf_final')(net['mbox_conf'])
+    net['predictions'] = Concatenate(name='predictions', axis=2)([net['mbox_loc'],
+                               net['mbox_conf'],
+                               net['mbox_priorbox']])
     # # Dence 15
     # net['fc15_1'] = Flatten(name='fc15_1')(net['conv14_3'])
     # net['fc15_2'] = Dense(input_shape[0], activation='relu',
@@ -141,7 +269,7 @@ def MSChromNet(input_shape):
     #                                 name='fc15_3')(net['fc15_2'])
     
     # Prediction
-    net['predictions'] = net['flatten15']    
+    # net['predictions'] = net['flatten15']    
     model = Model(net['input'], net['predictions'])
     return model
 
---
+++ ssd_layers.py
@@ -0,0 +1,198 @@
+"""Some special pupropse layers for SSD."""
+
+import keras.backend as K
+from keras.engine.topology import InputSpec
+from keras.engine.topology import Layer
+import numpy as np
+import tensorflow as tf
+
+import pickle
+import os
+
+class Normalize(Layer):
+    """Normalization layer as described in ParseNet paper.
+
+    # Arguments
+        scale: Default feature scale.
+
+    # Input shape
+        4D tensor with shape:
+        `(samples, channels, rows, cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(samples, rows, cols, channels)` if dim_ordering='tf'.
+
+    # Output shape
+        Same as input
+
+    # References
+        http://cs.unc.edu/~wliu/papers/parsenet.pdf
+
+    #TODO
+        Add possibility to have one scale for all features.
+    """
+    def __init__(self, scale, **kwargs):
+        if K.image_dim_ordering() == 'tf':
+            self.axis = 3
+        else:
+            self.axis = 1
+        self.scale = scale
+        super(Normalize, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(shape=input_shape)]
+        shape = (input_shape[self.axis],)
+        init_gamma = self.scale * np.ones(shape)
+        self.gamma = K.variable(init_gamma, name='{}_gamma'.format(self.name))
+        self.trainable_weights = [self.gamma]
+
+    def call(self, x, mask=None):
+        output = K.l2_normalize(x, self.axis)
+        output *= self.gamma
+        return output
+
+
+class PriorBox(Layer):
+    """Generate the prior boxes of designated sizes and aspect ratios.
+
+    # Arguments
+        img_size: Size of the input image as tuple (w, h).
+        min_size: Minimum box size in pixels.
+        max_size: Maximum box size in pixels.
+        aspect_ratios: List of aspect ratios of boxes.
+        flip: Whether to consider reverse aspect ratios.
+        variances: List of variances for x, y, w, h.
+        clip: Whether to clip the prior's coordinates
+            such that they are within [0, 1].
+
+    # Input shape
+        4D tensor with shape:
+        `(samples, channels, rows, cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(samples, rows, cols, channels)` if dim_ordering='tf'.
+
+    # Output shape
+        3D tensor with shape:
+        (samples, num_boxes, 8)
+
+    # References
+        https://arxiv.org/abs/1512.02325
+
+    #TODO
+        Add possibility not to have variances.
+        Add Theano support
+    """
+    def __init__(self, img_size, min_size, max_size=None, aspect_ratios=None,
+                 flip=True, variances=[0.1], clip=True, **kwargs):
+        if K.image_dim_ordering() == 'tf':
+            self.waxis = 2
+            self.haxis = 1
+        else:
+            self.waxis = 3
+            self.haxis = 2
+        self.img_size = img_size
+        if min_size <= 0:
+            raise Exception('min_size must be positive.')
+        self.min_size = min_size
+        self.max_size = max_size
+        self.aspect_ratios = [1.0]
+        if max_size:
+            if max_size < min_size:
+                raise Exception('max_size must be greater than min_size.')
+            self.aspect_ratios.append(1.0)
+        if aspect_ratios:
+            for ar in aspect_ratios:
+                if ar in self.aspect_ratios:
+                    continue
+                self.aspect_ratios.append(ar)
+                if flip:
+                    self.aspect_ratios.append(1.0 / ar)
+        self.variances = np.array(variances)
+        self.clip = True
+        super(PriorBox, self).__init__(**kwargs)
+
+    def compute_output_shape(self, input_shape):
+        num_priors_ = len(self.aspect_ratios)
+        layer_width = input_shape[self.waxis]
+        layer_height = input_shape[self.haxis]
+        num_boxes = num_priors_ * layer_width * layer_height
+        return (input_shape[0], num_boxes, 8)
+
+    def call(self, x, mask=None):
+        if hasattr(x, '_keras_shape'):
+            input_shape = x._keras_shape
+        elif hasattr(K, 'int_shape'):
+            input_shape = K.int_shape(x)
+        layer_width = input_shape[self.waxis]
+        layer_height = input_shape[self.haxis]
+        img_width = self.img_size[0]
+        img_height = self.img_size[1]
+        # define prior boxes shapes
+        box_widths = []
+        box_heights = []
+        for ar in self.aspect_ratios:
+            if ar == 1 and len(box_widths) == 0:
+                box_widths.append(self.min_size)
+                box_heights.append(self.min_size)
+            elif ar == 1 and len(box_widths) > 0:
+                box_widths.append(np.sqrt(self.min_size * self.max_size))
+                box_heights.append(np.sqrt(self.min_size * self.max_size))
+            elif ar != 1:
+                box_widths.append(self.min_size * np.sqrt(ar))
+                box_heights.append(self.min_size / np.sqrt(ar))
+        box_widths = 0.5 * np.array(box_widths)
+        box_heights = 0.5 * np.array(box_heights)
+        # define centers of prior boxes
+        step_x = img_width / layer_width # レイヤー上の１ポイントがカバーするオリジナル画像上のピクセル数（layer_width=19, img_width=300ならstep_x=15.78）
+        step_y = img_height / layer_height
+        linx = np.linspace(0.5 * step_x, img_width - 0.5 * step_x,
+                           layer_width) # img_width=300, layer_width=19 なら0-300の区間を19に分けた時のピクセル中心位置の数列（7.89, 23,68, ..., 292.105）
+        liny = np.linspace(0.5 * step_y, img_height - 0.5 * step_y,
+                           layer_height)
+        centers_x, centers_y = np.meshgrid(linx, liny)
+        centers_x = centers_x.reshape(-1, 1)
+        centers_y = centers_y.reshape(-1, 1)
+        # define xmin, ymin, xmax, ymax of prior boxes
+        num_priors_ = len(self.aspect_ratios)
+        prior_boxes = np.concatenate((centers_x, centers_y), axis=1)
+        prior_boxes = np.tile(prior_boxes, (1, 2 * num_priors_)) # 「1, 」が必要かどうかはよくわからない…1ならなくても結果は同じ？それとも次元が一つ増える？
+        prior_boxes[:, ::4] -= box_widths
+        prior_boxes[:, 1::4] -= box_heights
+        prior_boxes[:, 2::4] += box_widths
+        prior_boxes[:, 3::4] += box_heights
+        prior_boxes[:, ::2] /= img_width
+        prior_boxes[:, 1::2] /= img_height
+        prior_boxes = prior_boxes.reshape(-1, 4)
+        if self.clip: # prior_boxのxmin, ymin, xmax, ymaxは0-1でクリップしておく
+            prior_boxes = np.minimum(np.maximum(prior_boxes, 0.0), 1.0)
+        # define variances
+        num_boxes = len(prior_boxes)
+        if len(self.variances) == 1:
+            variances = np.ones((num_boxes, 4)) * self.variances[0]
+        elif len(self.variances) == 4:
+            variances = np.tile(self.variances, (num_boxes, 1)) # ここでvalianceを作る
+        else:
+            raise Exception('Must provide one or four variances.')
+        prior_boxes = np.concatenate((prior_boxes, variances), axis=1) # 作ったvalianceをconcatenateする shape: (priorboxのサイズ, 4+4)
+
+        """priorsを保存する"""
+        # temp_priors = []
+        # if os.path.exists('priors_test.pkl'):
+        #     with open('priors_test.pkl', mode='rb') as f:
+        #         temp_priors = pickle.load(f)
+        
+        # if len(temp_priors) != 0:
+        #     temp_priors = np.concatenate((temp_priors, prior_boxes), axis=0)
+        # else:
+        #     temp_priors = prior_boxes
+
+        # with open('priors_test.pkl', mode='wb') as f:
+        #     pickle.dump(temp_priors, f)
+
+        prior_boxes_tensor = K.expand_dims(K.variable(prior_boxes), 0) # バックエンドテンソルに変換（１次元追加）shape:TensorShape([Dimension(1), Dimension(54), Dimension(8)])
+        if K.backend() == 'tensorflow':
+            pattern = [tf.shape(x)[0], 1, 1] # patternのshapeは(none, 1, 1)的な感じ。tf.shape(x)[0]はバッチ数
+            prior_boxes_tensor = tf.tile(prior_boxes_tensor, pattern) # TensorShape([Dimension(None), Dimension(54), Dimension(8)]) これはバッチ数だけタイルされた形（バッチ数はNoneで予約）
+        elif K.backend() == 'theano':
+            #TODO
+            pass
+        return prior_boxes_tensor
