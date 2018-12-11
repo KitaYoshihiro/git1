21004521e3680719c08fcb7e4ca59650e6590f9f
Yoshihiro Kita <kita.yoshihiro@gmail.com>
2018-12-11 19:09:59 +0900

    a

--- mschrom_unet.py
+++ mschrom_unet.py
@@ -135,49 +135,50 @@ def MSChromUNet(input_shape, depth=0, u_net=True, peak_detector=True, num_classe
     net['autoencoder_flatten'] = Flatten(name='autoencoder_flatten')(x)
 
     # Gather Predictions
-    net['mbox_loc'] = Concatenate(name='mbox_loc', axis=1)([
-                            net['L1_mbox_loc_flat'],
-                            net['L2_mbox_loc_flat'],
-                            net['L3_mbox_loc_flat'],
-                            net['L4_mbox_loc_flat'],
-                            net['L5_mbox_loc_flat'],
-                            net['L6_mbox_loc_flat'],
-                            net['L7_mbox_loc_flat'],
-                            net['L8_mbox_loc_flat']])
-    net['mbox_conf'] = Concatenate(name='mbox_conf', axis=1)([
-                            net['L1_mbox_conf_flat'],
-                            net['L2_mbox_conf_flat'],
-                            net['L3_mbox_conf_flat'],
-                            net['L4_mbox_conf_flat'],
-                            net['L5_mbox_conf_flat'],
-                            net['L6_mbox_conf_flat'],
-                            net['L7_mbox_conf_flat'],
-                            net['L8_mbox_conf_flat']])
-    net['mbox_priorbox'] = Concatenate(name='mbox_priorbox', axis=1)([
-                            net['L1_mbox_priorbox'],
-                            net['L2_mbox_priorbox'],
-                            net['L3_mbox_priorbox'],
-                            net['L4_mbox_priorbox'],
-                            net['L5_mbox_priorbox'],
-                            net['L6_mbox_priorbox'],
-                            net['L7_mbox_priorbox'],
-                            net['L8_mbox_priorbox']])
-    if hasattr(net['mbox_loc'], '_keras_shape'):
-        num_boxes = net['mbox_loc']._keras_shape[-1] // 2
-    elif hasattr(net['mbox_loc'], 'int_shape'):
-        num_boxes = K.int_shape(net['mbox_loc'])[-1] // 2
-    net['mbox_loc'] = Reshape((num_boxes, 2),
-                              name='mbox_loc_final')(net['mbox_loc'])
-    net['mbox_conf'] = Reshape((num_boxes, num_classes),
-                               name='mbox_conf_logits')(net['mbox_conf'])
-    net['mbox_conf'] = Activation('softmax',
-                                  name='mbox_conf_final')(net['mbox_conf'])
-    net['predictions'] = Concatenate(name='predictions', axis=2)([net['mbox_loc'],
-                               net['mbox_conf'],
-                               net['mbox_priorbox']])
-
-    # Prediction
-    # net['predictions'] = net['autoencoder_flatten']
+    if peak_detector:        
+        net['mbox_loc'] = Concatenate(name='mbox_loc', axis=1)([
+                                net['L1_mbox_loc_flat'],
+                                net['L2_mbox_loc_flat'],
+                                net['L3_mbox_loc_flat'],
+                                net['L4_mbox_loc_flat'],
+                                net['L5_mbox_loc_flat'],
+                                net['L6_mbox_loc_flat'],
+                                net['L7_mbox_loc_flat'],
+                                net['L8_mbox_loc_flat']])
+        net['mbox_conf'] = Concatenate(name='mbox_conf', axis=1)([
+                                net['L1_mbox_conf_flat'],
+                                net['L2_mbox_conf_flat'],
+                                net['L3_mbox_conf_flat'],
+                                net['L4_mbox_conf_flat'],
+                                net['L5_mbox_conf_flat'],
+                                net['L6_mbox_conf_flat'],
+                                net['L7_mbox_conf_flat'],
+                                net['L8_mbox_conf_flat']])
+        net['mbox_priorbox'] = Concatenate(name='mbox_priorbox', axis=1)([
+                                net['L1_mbox_priorbox'],
+                                net['L2_mbox_priorbox'],
+                                net['L3_mbox_priorbox'],
+                                net['L4_mbox_priorbox'],
+                                net['L5_mbox_priorbox'],
+                                net['L6_mbox_priorbox'],
+                                net['L7_mbox_priorbox'],
+                                net['L8_mbox_priorbox']])
+        if hasattr(net['mbox_loc'], '_keras_shape'):
+            num_boxes = net['mbox_loc']._keras_shape[-1] // 2
+        elif hasattr(net['mbox_loc'], 'int_shape'):
+            num_boxes = K.int_shape(net['mbox_loc'])[-1] // 2
+        net['mbox_loc'] = Reshape((num_boxes, 2),
+                                name='mbox_loc_final')(net['mbox_loc'])
+        net['mbox_conf'] = Reshape((num_boxes, num_classes),
+                                name='mbox_conf_logits')(net['mbox_conf'])
+        net['mbox_conf'] = Activation('softmax',
+                                name='mbox_conf_final')(net['mbox_conf'])
+        net['predictions'] = Concatenate(name='predictions', axis=2)([net['mbox_loc'],
+                                net['mbox_conf'],
+                                net['mbox_priorbox']])
+    else:
+        net['predictions'] = net['autoencoder_flatten']
+        
     model = Model(net['input'], net['predictions'])
     return model
 
